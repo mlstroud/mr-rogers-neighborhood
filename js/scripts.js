8f09d3e3eee5d2bcc61439842255aac3ff00b55f
Matt Stroud <lixingke06@gmail.com>
2020-04-03 09:22:27 -0700

    add javascript to display results for testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+function getOutput(input) {
+  return input;
+};
+
+
 $(document).ready(function() {
 
+  $("#get-number").submit(function(event) {
+    var input = $("#input-number").val();
+    var output = getOutput(input);
+  
+    $("#result").text(output);
+    event.preventDefault();
+  });
 });
